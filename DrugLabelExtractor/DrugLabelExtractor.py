ba93242e36cbb1bd95d0ae99e60e12f39e1e3957
K0ykwon <yko081524@yonsei.ac.kr>
2025-04-14 20:02:54 +0900

    Update DrugLabelExtractor.py

--- DrugLabelExtractor/DrugLabelExtractor.py
+++ DrugLabelExtractor/DrugLabelExtractor.py
@@ -76,7 +76,7 @@ class DrugLabelExtractor:
         
         
         response = self.openai_client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4o-mini",
             messages=[
                 {
                     "role": "system",
